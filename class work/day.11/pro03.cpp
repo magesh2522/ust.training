bf0414ace2823bf7bf499495d7324ecac2a9a758
magesh2522 <Administrator@53C107A59C1D551>
2025-04-30 16:56:03 +0530

    update

--- class work/day.11/pro03.cpp
+++ class work/day.11/pro03.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 using namespace std;
+void displayptr(int[],int);
+void update(int[],int);
+
 
 
 int main() {
-	char arr1[3][3] = { { 'A', 'B', 'C' }, { 'D', 'E', 'F' }, { 'G', 'H', 'I' } };
-	cout<< "Address of arr1[0][0] = " << (long int)&arr1[0][0] << endl;
+	int a[] = { 1,3,5,7,2,4,6,8 };
+	int n = sizeof(a) / sizeof(a[0]);
+	displayptr(a, n);
+	update(a, 3);
+
+	displayptr(a, n);
+	return 0;
 }
-
-
-
+void displayptr(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << a[i] <<endl;
+	}
+	cout << endl;
+}
+void update(int a[],int pos) {
+	a[pos] = 444;
+	}
